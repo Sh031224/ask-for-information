75822902f5c48bd601f46d7bf97fcce42f53d8ae
Sh031224 <1cktmdgh2@gmail.com>
2021-04-06 16:12:58 +0900

    Fix: 메인 페이지

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,95 +1,82 @@
-import React from 'react';
-import clsx from 'clsx';
-import Layout from '@theme/Layout';
-import Link from '@docusaurus/Link';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import useBaseUrl from '@docusaurus/useBaseUrl';
-import styles from './styles.module.css';
+import React from "react";
+import clsx from "clsx";
+import Layout from "@theme/Layout";
+import Link from "@docusaurus/Link";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import useBaseUrl from "@docusaurus/useBaseUrl";
+import styles from "./styles.module.css";
 
 const features = [
-  {
-    title: 'Easy to Use',
-    imageUrl: 'img/undraw_docusaurus_mountain.svg',
-    description: (
-      <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
-      </>
-    ),
-  },
-  {
-    title: 'Focus on What Matters',
-    imageUrl: 'img/undraw_docusaurus_tree.svg',
-    description: (
-      <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
-      </>
-    ),
-  },
-  {
-    title: 'Powered by React',
-    imageUrl: 'img/undraw_docusaurus_react.svg',
-    description: (
-      <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
-      </>
-    ),
-  },
+	{
+		title: "기초부터 천천히",
+		imageUrl: "img/undraw_docusaurus_mountain.svg",
+		description: (
+			<>
+				<code>ask-for-information</code> 을 통하여 여러 언어 / 프레임워크의 기초부터 천천히 쌓을 수 있습니다.
+			</>
+		)
+	},
+	{
+		title: "모두 참여 가능",
+		imageUrl: "img/undraw_docusaurus_tree.svg",
+		description: (
+			<>
+				누구나 이 프로젝트에 <code>Github</code> 를 통하여 기여할 수 있습니다.
+			</>
+		)
+	},
+	{
+		title: "경험 전달",
+		imageUrl: "img/undraw_docusaurus_react.svg",
+		description: <>기초적인 지식 뿐만아니라 이때까지 겪었던 경험도 같이 전달함으로써 보다 신뢰성 있는 정보를 제공합니다.</>
+	}
 ];
 
-function Feature({imageUrl, title, description}) {
-  const imgUrl = useBaseUrl(imageUrl);
-  return (
-    <div className={clsx('col col--4', styles.feature)}>
-      {imgUrl && (
-        <div className="text--center">
-          <img className={styles.featureImage} src={imgUrl} alt={title} />
-        </div>
-      )}
-      <h3>{title}</h3>
-      <p>{description}</p>
-    </div>
-  );
+function Feature({ imageUrl, title, description }) {
+	const imgUrl = useBaseUrl(imageUrl);
+	return (
+		<div className={clsx("col col--4", styles.feature)}>
+			{imgUrl && (
+				<div className="text--center">
+					<img className={styles.featureImage} src={imgUrl} alt={title} />
+				</div>
+			)}
+			<h3>{title}</h3>
+			<p>{description}</p>
+		</div>
+	);
 }
 
 export default function Home() {
-  const context = useDocusaurusContext();
-  const {siteConfig = {}} = context;
-  return (
-    <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
-      <header className={clsx('hero hero--primary', styles.heroBanner)}>
-        <div className="container">
-          <h1 className="hero__title">{siteConfig.title}</h1>
-          <p className="hero__subtitle">{siteConfig.tagline}</p>
-          <div className={styles.buttons}>
-            <Link
-              className={clsx(
-                'button button--outline button--secondary button--lg',
-                styles.getStarted,
-              )}
-              to={useBaseUrl('docs/')}>
-              Get Started
-            </Link>
-          </div>
-        </div>
-      </header>
-      <main>
-        {features && features.length > 0 && (
-          <section className={styles.features}>
-            <div className="container">
-              <div className="row">
-                {features.map((props, idx) => (
-                  <Feature key={idx} {...props} />
-                ))}
-              </div>
-            </div>
-          </section>
-        )}
-      </main>
-    </Layout>
-  );
+	const context = useDocusaurusContext();
+	const { siteConfig = {} } = context;
+	return (
+		<Layout title={`Hello from ${siteConfig.title}`} description="Description will go into a meta tag in <head />">
+			<header className={clsx("hero", styles.heroBanner)}>
+				<div className="container">
+					<img className={clsx(styles.heroBannerLogo, "margin-vert--md")} src={useBaseUrl("img/logo.svg")} />
+					<h1 className="hero__title">{siteConfig.title}</h1>
+					<p className="hero__subtitle">{siteConfig.tagline}</p>
+					<div className={styles.buttons}>
+						<Link className={clsx("button button--primary button--lg", styles.getStarted)} to={useBaseUrl("docs/")}>
+							문서 확인하기 →
+						</Link>
+					</div>
+				</div>
+			</header>
+			<main>
+				{features && features.length > 0 && (
+					<section className={styles.features}>
+						<div className="container">
+							<div className="row">
+								{features.map((props, idx) => (
+									<Feature key={idx} {...props} />
+								))}
+							</div>
+						</div>
+					</section>
+				)}
+			</main>
+		</Layout>
+	);
 }
