82ac9959c4e8132a8fb929821285ff0e39edfbed
Sh031224 <1cktmdgh2@gmail.com>
2021-04-20 13:34:54 +0900

    Fix: tab size

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,82 +1,82 @@
 /** @type {import('@docusaurus/types').DocusaurusConfig} */
 module.exports = {
-	title: "Ask for Information",
-	tagline: "함께 만들어가는 코딩 지식",
-	url: "https://sh031224.github.io",
-	baseUrl: "/ask-for-information/",
-	onBrokenLinks: "throw",
-	onBrokenMarkdownLinks: "warn",
-	favicon: "img/favicon.ico",
-	organizationName: "Sh031224", // Usually your GitHub org/user name.
-	projectName: "ask-for-information", // Usually your repo name.
-	themeConfig: {
-		navbar: {
-			title: "Ask-for-information",
-			logo: {
-				alt: "My Site Logo",
-				src: "img/logo.svg"
-			},
-			items: [
-				{
-					to: "docs/",
-					activeBasePath: "docs",
-					label: "문서",
-					position: "left"
-				},
-				{ to: "blog", label: "가이드", position: "left" },
-				{
-					href: "https://github.com/Sh031224/ask-for-information",
-					label: "GitHub",
-					position: "right"
-				}
-			]
-		},
-		footer: {
-			style: "dark",
-			links: [
-				{
-					title: "커뮤니티",
-					items: [
-						{
-							label: "시작하기",
-							to: "docs"
-						},
-						{
-							label: "건의사항 및 이슈",
-							href: "https://github.com/Sh031224/ask-for-information/issues/new"
-						}
-					]
-				},
-				{
-					title: "더 보기",
-					items: [
-						{
-							label: "가이드",
-							to: "blog"
-						},
-						{
-							label: "GitHub",
-							href: "https://github.com/Sh031224/ask-for-information"
-						}
-					]
-				}
-			],
-			copyright: `Copyright © ${new Date().getFullYear()} Ask-for-information, Inc. Built with Docusaurus.`
-		}
-	},
-	presets: [
-		[
-			"@docusaurus/preset-classic",
-			{
-				docs: {
-					sidebarPath: require.resolve("./sidebars.js"),
-					// Please change this to your repo.
-					editUrl: "https://github.com/Sh031224/ask-for-information/edit/master/"
-				},
-				theme: {
-					customCss: require.resolve("./src/css/custom.css")
-				}
-			}
-		]
-	]
+  title: "Ask for Information",
+  tagline: "함께 만들어가는 코딩 지식",
+  url: "https://sh031224.github.io",
+  baseUrl: "/ask-for-information/",
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "img/favicon.ico",
+  organizationName: "Sh031224", // Usually your GitHub org/user name.
+  projectName: "ask-for-information", // Usually your repo name.
+  themeConfig: {
+    navbar: {
+      title: "Ask-for-information",
+      logo: {
+        alt: "My Site Logo",
+        src: "img/logo.svg"
+      },
+      items: [
+        {
+          to: "docs/",
+          activeBasePath: "docs",
+          label: "문서",
+          position: "left"
+        },
+        { to: "blog", label: "가이드", position: "left" },
+        {
+          href: "https://github.com/Sh031224/ask-for-information",
+          label: "GitHub",
+          position: "right"
+        }
+      ]
+    },
+    footer: {
+      style: "dark",
+      links: [
+        {
+          title: "커뮤니티",
+          items: [
+            {
+              label: "시작하기",
+              to: "docs"
+            },
+            {
+              label: "건의사항 및 이슈",
+              href: "https://github.com/Sh031224/ask-for-information/issues/new"
+            }
+          ]
+        },
+        {
+          title: "더 보기",
+          items: [
+            {
+              label: "가이드",
+              to: "blog"
+            },
+            {
+              label: "GitHub",
+              href: "https://github.com/Sh031224/ask-for-information"
+            }
+          ]
+        }
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} Ask-for-information, Inc. Built with Docusaurus.`
+    }
+  },
+  presets: [
+    [
+      "@docusaurus/preset-classic",
+      {
+        docs: {
+          sidebarPath: require.resolve("./sidebars.js"),
+          // Please change this to your repo.
+          editUrl: "https://github.com/Sh031224/ask-for-information/edit/master/"
+        },
+        theme: {
+          customCss: require.resolve("./src/css/custom.css")
+        }
+      }
+    ]
+  ]
 };
--- sidebars.js
+++ sidebars.js
@@ -1,25 +1,25 @@
 module.exports = {
-	docs: [
-		{
-			type: "category",
-			label: "시작하기",
-			items: ["start/start-start"]
-		},
-		{
-			type: "category",
-			label: "면접 질문",
-			items: ["interview/interview-frontend", "interview/interview-backend", "interview/interview-android"]
-		},
-		{
-			type: "category",
-			label: "웹",
-			items: [
-				{
-					type: "category",
-					label: "HTML",
-					items: ["web/html/web-html-doctype"]
-				}
-			]
-		}
-	]
+  docs: [
+    {
+      type: "category",
+      label: "시작하기",
+      items: ["start/start-start"]
+    },
+    {
+      type: "category",
+      label: "면접 질문",
+      items: ["interview/interview-frontend", "interview/interview-backend", "interview/interview-android"]
+    },
+    {
+      type: "category",
+      label: "웹",
+      items: [
+        {
+          type: "category",
+          label: "HTML",
+          items: ["web/html/web-html-doctype"]
+        }
+      ]
+    }
+  ]
 };
